9306eb3026a1070d3fe0f0cecfff6b4f6d57c8ff
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-20 11:01:10 +0100

    Ho implemntato il codice

--- CaesarCipherLibrary/CaesarCipher.cs
+++ CaesarCipherLibrary/CaesarCipher.cs
@@ -4,5 +4,31 @@ namespace CaesarCipherLibrary
 {
     public class CaesarCipher
     {
+        protected int Key { get; set; } // Dichiaro una chiave
+
+        private char Cipher(char ch) // 
+        {
+            if (!char.IsLetter(ch)) // 
+                return ch;
+
+            char offset; // 
+
+            if (char.IsUpper(ch)) // controllo se il carattere è maiuscolo o minuscolo
+                offset = 'A'; // 
+            else // 
+                offset = 'a';
+
+            return (char)(((ch - offset + Key) % 26) + offset); // 
+        }
+
+        protected string DoWork (string m) // 
+        {
+            string message = string.Empty; // 
+
+            foreach (char ch in m) // 
+                message += Cipher(ch); // 
+
+            return message; // 
+        }
     }
 }
